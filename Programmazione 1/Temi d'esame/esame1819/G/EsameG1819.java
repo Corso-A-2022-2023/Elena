14487dec7ffbdb4953fc47268ed845c816af9505
Ela <64909186+Ela17@users.noreply.github.com>
2022-12-20 18:35:18 +0100

    Fatti esami G e H

--- Programmazione 1/Temi d'esame/esame1819/G/EsameG1819.java
+++ Programmazione 1/Temi d'esame/esame1819/G/EsameG1819.java
@@ -1,5 +1,5 @@
 /**
- * ESAME PROGRAMMAZIONE 1 08/07/19
+ * ESAME PROGRAMMAZIONE 1 10/06/19
  * Matricola: SCRIVERLA ORA!!!
  * Cognome: SCRIVERLO ORA!!!
  * Nome: SCRIVERLO ORA!!!
@@ -11,42 +11,80 @@
  * PER CONSEGNARE/RITIRARSI chiamare un docente.
  * SE QUESTA CLASSE NON COMPILA E' INUTILE CONSEGANRE.
  */
-public class EsameH1819 {
+public class EsameG1819 {
 
 	/** ESERCIZIO 1 (Massimo 7 punti -- da consegnare elettronicamente). 
 	 * Scrivere un metodo iterativo e1 con le seguenti caratteristiche:
 	 * -) e1 ha due parametri, entrambi di tipo array di interi, di nome 
 	 * a e b, rispettivamente; 
-	 * -) e1 restituisce true se ogni elemento di a e' 
-	 * inferiore a non meno di due elementi di posizione dispari in b. 
+	 * -) e1 restituisce true se esistono almeno due elementi di a 
+	 * inferiori ad ogni elemento di posizione dispari in b. 
 	 * In ogni altro caso, e1 restituisce false. 
 	 * SCRIVERE la soluzione immediatamente qui sotto.                */
+	 
+	public static boolean e1(int[] a, int[] b){
+		boolean  esiste = false;
+		
+		if(a != null && b != null && a.length > 1 && b.length > 0){
+			int cnt = 0;
+			for(int i = 0; i < a.length && !esiste; i++){
+				boolean minore = true;
+				for(int j = 1; j < b.length && minore; j += 2){	// salto di 2 per controllare solo j dispari
+					minore = a[i] < b[j];
+				}
+				if (minore)
+					cnt++;
+				if(cnt==2)
+					esiste = true;
+			}
+		}
+		
+		return esiste;
+	}
 
 	/** ESERCIZIO 2 (Massimo 7 punti -- da consegnare elettronicamente).
 	 * Scrivere un metodo involucro (wrapper o guscio) e2 tale che:
      * -) e2 ha come unico parametro formale un array di interi a;
      * -) e2 restituisce un array di interi, richiamando un metodo 
-     * ricorsivo controvariante e2R che si comporta come segue:
+     * ricorsivo dicotomico e2R che si comporta come segue:
      * -) e2R prende quattro parametri:
      * --) il primo parametro e' l'array a;
      * --) il secondo parametro e' un array b, passatogli da e2, che
-     * verra' restituito come risultato;
-     * --) il terzo parametro e' l'indice che individua 
-     * l'intervallo di lavoro sull'array a;
-     * --) il quarto parametro e' l'indice che individua il prossimo
-     * elemento di b in cui inserire un valore. 
-     * -) e2R scrive nell'array b, 
-     * ogni elemento di a di posizione pari, nell'ordine in cui compare.
-     * Quindi, in generale, b contiene meno elementi di quelli di a.
-     * Per metodo ricorsivo *controvariante* intendiamo che il parametro 
-     * che guida la ricorsione cresce al decrescere della
-     * difficolta' della porzione di problema che resta da risolvere.  
+     * conterra' il risulato;
+     * --) il terzo ed il quarto parametro sono gli indici che individuano 
+     * l'intervallo di lavoro sull'array a; 
+     * -) e2R scrive nell'array b ogni elemento di a, 
+     * incrementato di una unita' se l'elemento ha posizione pari, 
+     * decrementato di una unita' se l'elemento ha posizione dispari.
 	 * Scrivere la soluzione immediatamente qui sotto.                */
+	 
+	public static int[] e2(int[] a){
+		if(a == null)
+			return null;
+		int[] b = new int[a.length];
+		if(a.length > 0)
+			e2R(a, b, 0, a.length-1);	
+		return b;
+	}
+	
+	public static void e2R(int[] a, int[] b, int l, int r){
+		if(l == r){
+			if(l%2 == 0)
+				b[l] = a[l] + 1;
+			else
+				b[l] = a[l] - 1;
+		}
+		else{
+			int m = (l+r)/2;
+			e2R(a, b, l, m);
+			e2R(a, b, m+1, r);
+		}
+	}
 	 
 	/** ESERCIZIO 3 (Massimo 2 + 2 + 3 + 3 punti -- da consegnare a mano)	 
      * Sia P(i) il seguente predicato:
      * 
-	 *   "true&a[0]&&...&&a[j-2]==r all'inizio dell'iterazione i".
+	 *   "false||a[0]||...||a[j-2]==r vero alla fine dell'iterazione i".
 	 *   
 	 * Dato il metodo e3, dimostrare che, per ogni valore i>=0,
 	 * il predicato P(i) e' vero, usando il principio di induzione:
@@ -56,13 +94,13 @@ public class EsameH1819 {
 	 * 4) dimostrare che il predicato al punto 2 e' vero (3 pt.)      
 	 */
 	public static boolean e3(boolean[] a) {
-		boolean r = true;
+		boolean r = false;
 		int j = 1;
 		// prima di ogni iterazione
 		while (j-1 < a.length) {
 			// inizio dell'iterazione i >= 0
-			j = j+1;
-			r = r&&a[j-2];
+			j = j + 1;
+			r = r || a[j-2];
 			// inizio dell'iterazione i+1 > 0
 		}
 		return r;
@@ -72,20 +110,18 @@ public class EsameH1819 {
 	 * Scrivere lo stato della memoria della JVM giusto prima della 
 	 * disallocazione del frame di attivazione del metodo m in cui 
 	 * valore di i e' 0.                                            */
-
 	public static void m(boolean[] a, int i, boolean[] b) {
-		if (i == 0) {
-			b[b.length - 1] = true;
-		} else {
+		if (i > 0) {
 			b[i - 1] = a[i];
-			m(a, i - 1, b);  // indirizzo ritorno (B)
-		}
+			m(a, i - 1, b);  // indirizzo ritorno (b)
+		} else
+			b[b.length - 1] = true;
 	}
 
 	/******************************************/
 	public static void main(String[] args) {
 		boolean[] a = { true, false, true };
 		boolean[] b = new boolean[a.length];
-		m(a, a.length - 1, b); // indirizzo ritorno (A)
+		m(a, a.length - 1, b); // indirizzo ritorno (a)
 	}
 }
--- Programmazione 1/Temi d'esame/esame1819/H/EsameH1819.java
+++ Programmazione 1/Temi d'esame/esame1819/H/EsameH1819.java
@@ -1,5 +1,5 @@
 /**
- * ESAME PROGRAMMAZIONE 1 10/06/19
+ * ESAME PROGRAMMAZIONE 1 08/07/19
  * Matricola: SCRIVERLA ORA!!!
  * Cognome: SCRIVERLO ORA!!!
  * Nome: SCRIVERLO ORA!!!
@@ -11,37 +11,83 @@
  * PER CONSEGNARE/RITIRARSI chiamare un docente.
  * SE QUESTA CLASSE NON COMPILA E' INUTILE CONSEGANRE.
  */
-public class EsameG1819 {
+public class EsameH1819 {
 
 	/** ESERCIZIO 1 (Massimo 7 punti -- da consegnare elettronicamente). 
 	 * Scrivere un metodo iterativo e1 con le seguenti caratteristiche:
 	 * -) e1 ha due parametri, entrambi di tipo array di interi, di nome 
 	 * a e b, rispettivamente; 
-	 * -) e1 restituisce true se esistono almeno due elementi di a 
-	 * inferiori ad ogni elemento di posizione dispari in b. 
+	 * -) e1 restituisce true se ogni elemento di a e' 
+	 * inferiore a non meno di due elementi di posizione dispari in b. 
 	 * In ogni altro caso, e1 restituisce false. 
 	 * SCRIVERE la soluzione immediatamente qui sotto.                */
-	 
+	
+	public static boolean e1(int[] a, int[] b){
+		boolean ogniElem = true;
+		
+		if(a == null || b == null || a.length == 0 || b.length <= 1)
+			ogniElem = false;
+		else{
+			for(int i = 0; i < a.length && ogniElem; i++){
+				boolean inferiore = true;
+				int cnt = 0;
+				for(int j = 1; j < b.length && cnt < 2; j += 2){	// parto da j=1 e salto ogni 2 per controllare solo elem dispari
+					inferiore = a[i] < b[j];
+					if(inferiore)
+						cnt++;
+				}
+				if(cnt < 2)	// se non ho trovato almeno 2 elementi per la riga i allora false ed esco
+					ogniElem = false;
+			}
+		}
+		
+		return ogniElem;
+	}
+
 	/** ESERCIZIO 2 (Massimo 7 punti -- da consegnare elettronicamente).
 	 * Scrivere un metodo involucro (wrapper o guscio) e2 tale che:
      * -) e2 ha come unico parametro formale un array di interi a;
      * -) e2 restituisce un array di interi, richiamando un metodo 
-     * ricorsivo dicotomico e2R che si comporta come segue:
+     * ricorsivo controvariante e2R che si comporta come segue:
      * -) e2R prende quattro parametri:
      * --) il primo parametro e' l'array a;
      * --) il secondo parametro e' un array b, passatogli da e2, che
-     * conterra' il risulato;
-     * --) il terzo ed il quarto parametro sono gli indici che individuano 
-     * l'intervallo di lavoro sull'array a; 
-     * -) e2R scrive nell'array b ogni elemento di a, 
-     * incrementato di una unita' se l'elemento ha posizione pari, 
-     * decrementato di una unita' se l'elemento ha posizione dispari.
+     * verra' restituito come risultato;
+     * --) il terzo parametro e' l'indice che individua 
+     * l'intervallo di lavoro sull'array a;
+     * --) il quarto parametro e' l'indice che individua il prossimo
+     * elemento di b in cui inserire un valore. 
+     * -) e2R scrive nell'array b ogni elemento di a di posizione pari, 
+	 *	  nell'ordine in cui compare.
+     * Quindi, in generale, b contiene meno elementi di quelli di a.
+     * Per metodo ricorsivo *controvariante* intendiamo che il parametro 
+     * che guida la ricorsione cresce al decrescere della
+     * difficolta' della porzione di problema che resta da risolvere.  
 	 * Scrivere la soluzione immediatamente qui sotto.                */
-	 
+	
+	public static int[] e2(int[] a){
+		if(a == null)
+			return null;
+		int n;
+		n = (a.length % 2 == 0) ? a.length/2 : a.length/2 + 1;
+		int[] b = new int[n];
+		e2R(a, b, 0, 0);
+		return b;
+	}
+	
+	public static void e2R(int[] a, int[] b, int i, int j){
+		// caso base i == a.length sottinteso
+		if(i <= a.length - 1 && j <= b.length - 1){
+			if(i%2 == 0)
+				b[j] = a[i];
+			e2R(a, b, i+2, j+1);
+		}
+	}
+	
 	/** ESERCIZIO 3 (Massimo 2 + 2 + 3 + 3 punti -- da consegnare a mano)	 
      * Sia P(i) il seguente predicato:
      * 
-	 *   "false||a[0]||...||a[j-2]==r vero alla fine dell'iterazione i".
+	 *   "true&a[0]&&...&&a[j-2]==r all'inizio dell'iterazione i".
 	 *   
 	 * Dato il metodo e3, dimostrare che, per ogni valore i>=0,
 	 * il predicato P(i) e' vero, usando il principio di induzione:
@@ -51,13 +97,13 @@ public class EsameG1819 {
 	 * 4) dimostrare che il predicato al punto 2 e' vero (3 pt.)      
 	 */
 	public static boolean e3(boolean[] a) {
-		boolean r = false;
+		boolean r = true;
 		int j = 1;
 		// prima di ogni iterazione
 		while (j-1 < a.length) {
 			// inizio dell'iterazione i >= 0
-			j = j + 1;
-			r = r || a[j-2];
+			j = j+1;
+			r = r&&a[j-2];
 			// inizio dell'iterazione i+1 > 0
 		}
 		return r;
@@ -67,18 +113,20 @@ public class EsameG1819 {
 	 * Scrivere lo stato della memoria della JVM giusto prima della 
 	 * disallocazione del frame di attivazione del metodo m in cui 
 	 * valore di i e' 0.                                            */
+
 	public static void m(boolean[] a, int i, boolean[] b) {
-		if (i > 0) {
-			b[i - 1] = a[i];
-			m(a, i - 1, b);  // indirizzo ritorno (b)
-		} else
+		if (i == 0) {
 			b[b.length - 1] = true;
+		} else {
+			b[i - 1] = a[i];
+			m(a, i - 1, b);  // indirizzo ritorno (B)
+		}
 	}
 
 	/******************************************/
 	public static void main(String[] args) {
 		boolean[] a = { true, false, true };
 		boolean[] b = new boolean[a.length];
-		m(a, a.length - 1, b); // indirizzo ritorno (a)
+		m(a, a.length - 1, b); // indirizzo ritorno (A)
 	}
 }
